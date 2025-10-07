ffb7ce6af8674c8998752e9c1ff349e3240ca9f2
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2025-10-07 13:57:31 +0100

    completed the UI design for the home page

--- frontend/coding-interview/src/App.jsx
+++ frontend/coding-interview/src/App.jsx
@@ -1,20 +1,13 @@
-// import { useState } from 'react'
-import {Routes, Route} from "react-router"
-import { HomePage } from "./components/HomePage"
-import { HistoryPanel } from "./components/History"
+import { Header } from "./components/Header"
+import { Body } from "./components/Body"
 import './App.css'
-import { LoginPage } from "./components/LoginPage"
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path="history" element={<HistoryPanel />}/>
-    </Routes>
-  
+    <>
+      <Header />
+      <Body />
+    </>
   )
 }
 
--- frontend/coding-interview/src/components/Header.jsx
+++ frontend/coding-interview/src/components/Header.jsx
@@ -1,59 +1,14 @@
-import "./Header.css";
-import { useState, useEffect, useRef } from "react";
-import { Link } from "react-router";
-import ProfileImage from "../assets/female.png";
-
+import './Header.css'
+import ProfileImage from '../assets/female.png'
 export function Header() {
-  const [showDropDown, setShowDropdown] = useState("");
-  const dropdownRef = useRef(null);
-
-
-  const toggleDropDown = () => {
-    setShowDropdown((prev) => !prev);
-  };
-  // Close dropdown when clicking outside
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target)
-      ) {
-        setShowDropdown(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
   return (
-      // <div className="app-header">
-        <div className="header-content">
-          <h1>Code Challenge Generator</h1>
-
-          <nav>
-            <Link to="/">Generate Challenge</Link>
-            <Link to="/history">History</Link>
-            <div className="profile-section" ref={dropdownRef}>
-              <img
-                className="profile-image"
-                src={ProfileImage}
-                alt="profile-image"
-                onClick={toggleDropDown}
-              />
-              {showDropDown && (
-                <div className="dropdown-menu">
-                  <Link to="/login">Login</Link>
-                  <Link to="/signup">Signup</Link>
-                  <Link to="/profile">Profile</Link>
-                  <Link to="/signout">Signout</Link>
-                </div>
-              )}
-            </div>
-          </nav>
-        </div>
-      // </div>
-  );
+    <div className="header-container">
+      <h1 className='h2'>Code Challenge Generator</h1>
+      <nav>
+        <a href="">Generate Challenge</a>
+        <a href="">History</a>
+        <img src={ProfileImage} alt="profile-image" className='profile-image' />
+      </nav>
+    </div>
+  )
 }
